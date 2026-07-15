ee81bd77668b9186c611c9c381a3040a667d0a8d
Milk2904 <vominhdat65@gmail.com>
2026-07-15 20:00:39 +0700

    add 3 file code python

---
+++ src/evalution/metrics.py
@@ -0,0 +1,21 @@
+class Metrics:
+
+    @staticmethod
+    def latency():
+        pass
+
+    @staticmethod
+    def throughput():
+        pass
+
+    @staticmethod
+    def safety():
+        pass
+
+    @staticmethod
+    def quality():
+        pass
+
+    @staticmethod
+    def resource_usage():
+        pass
